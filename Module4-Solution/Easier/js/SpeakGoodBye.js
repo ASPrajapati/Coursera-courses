8d61167adae4ed4166bf23c20ee50b0f9e20eeb4
ASPrajapati <ajayp15036@gmail.com>
2020-05-17 15:26:17 +0530

    Module 4

---
+++ Module4-Solution/Easier/js/SpeakGoodBye.js
@@ -0,0 +1,9 @@
+(function (window){
+	var speakWord = "Good Bye";
+	var byeSpeaker = {};
+	byeSpeaker.speak = function (name) {
+		console.log(speakWord + " " + name);
+	};
+	byeSpeaker.speak("Ajay");
+	window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ Module4-Solution/Easier/js/script.js
@@ -0,0 +1,31 @@
+//console.log("/**************** First Type *****************/");
+/*console.log("");
+var names = ["Yaakov", "John", "Jen", "Jason", 
+"Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+	var firstletter = names[i].charAt(0).toLowerCase();
+	if(firstletter === "j"){
+		console.log("Good Bye " + names[i]);
+	}else{
+		console.log("Hello "+ names[i]);
+	}
+}
+*/
+
+//console.log("");
+//console.log("/**************** Second Type ****************");
+//console.log("");
+(function (){
+	var names = ["Yaakov", "John", "Jen", "Jason", 
+"Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+	var firstletter = names[i].charAt(0).toLowerCase();
+	if(firstletter === "j"){
+		console.log("Good Bye " + names[i]);
+	}else{
+		console.log("Hello "+ names[i]);
+	}
+}
+})();
+
+
