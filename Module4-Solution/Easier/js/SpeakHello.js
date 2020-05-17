42e0c601a17872d99740f8b9b7d62a24be9a0703
ASPrajapati <ajayp15036@gmail.com>
2020-05-17 16:25:59 +0530

    Module 4 Again added

---
+++ Module4-Solution/Easier/js/SpeakHello.js
@@ -0,0 +1,9 @@
+(function (window){
+	var speakWord = "Hello";
+	var helloSpeaker = {};
+	helloSpeaker.speak = function (name) {
+		console.log(speakWord + " " + name);
+	};
+	helloSpeaker.speak("Anil");
+	window.helloSpeaker = helloSpeaker;
+})(window);
